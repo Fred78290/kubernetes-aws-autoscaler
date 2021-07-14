664273d00c7ead936ead66e9e3d00dde5e30e54f
fboltz <frederic.boltz@gmail.com>
2021-07-14 17:33:37 +0200

    Upgrade to kubernetes v1.21.2

--- grpc/protoc_grpc.sh
+++ grpc/protoc_grpc.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-#/bin/bash
 
 PB_RELEASE="3.11.1"
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
@@ -13,9 +12,12 @@ mkdir -p $PROTOC_DIR
 
 pushd $PROTOC_DIR
 
-go get -v google.golang.org/grpc@v1.26.0
-go get -v github.com/golang/protobuf@v1.3.2
-go get -v github.com/golang/protobuf/protoc-gen-go@v1.3.2
+#go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+#go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+
+go get -v google.golang.org/grpc@v1.29.1
+#go get -v github.com/golang/protobuf@v1.3.2
+#go get -v github.com/golang/protobuf/protoc-gen-go@v1.3.2
 
 curl -LO ${PB_REL}/download/v${PB_RELEASE}/protoc-${PB_RELEASE}-linux-x86_64.zip
 unzip protoc-${PB_RELEASE}-linux-x86_64.zip
