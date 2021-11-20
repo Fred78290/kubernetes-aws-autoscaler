333b2888fea1af7a064cdf0db84f39ceec3404e2
fboltz <frederic.boltz@gmail.com>
2021-11-20 11:45:18 +0100

    FEAT: Moved to external github project

--- masterkube/bin/kubeconfig-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Delete context: k8s-$1-masterkube-admin@$1"
-
-kubectl config delete-context k8s-$1-masterkube-admin@$1
-
-echo
-echo "Delete context: $1"
-kubectl config delete-cluster $1
