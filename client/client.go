2bc19556e4b658960214b9ce6184b821948b7462
fboltz <frederic.boltz@gmail.com>
2022-09-27 14:02:45 +0200

    FEAT: Use node patch instead node update

--- client/client.go
+++ client/client.go
@@ -382,9 +382,23 @@ func (p *SingletonClientGenerator) SetProviderID(nodeName, providerID string) er
 			return true, nil
 		}
 
-		node.Spec.ProviderID = providerID
+		patch := utils.ToYAML(map[string]interface{}{
+			"kind":       "Node",
+			"apiVersion": "v1",
+			"spec": map[string]string{
+				"providerID": providerID,
+			},
+		})
 
-		if _, err = kubeclient.CoreV1().Nodes().Update(ctx, node, metav1.UpdateOptions{}); err != nil {
+		patchOptions := metav1.PatchOptions{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Node",
+				APIVersion: "v1",
+			},
+			FieldManager: "application/apply-patch",
+		}
+
+		if _, err = kubeclient.CoreV1().Nodes().Patch(ctx, nodeName, typesv1.ApplyPatchType, []byte(patch), patchOptions); err != nil {
 			glog.Warnf("Set providerID node:%s is not ready, err = %s", nodeName, err)
 			return false, nil
 		}
