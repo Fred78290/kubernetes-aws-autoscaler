61e78ba84e079e05444099c0a3dcb1fd56f70ba4
fboltz <frederic.boltz@gmail.com>
2022-12-24 18:01:58 +0100

    FIX: Local run

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 pushd $(dirname $0)
 
+sudo rm -rf out vendor
+
 make container
 
 GOARCH=$(go env GOARCH)
 
-./out/aws-autoscaler-$GOARCH \
-    --config=~/Projects/autoscaled-masterkube-aws/config/aws-ca-k8s/kubernetes-aws-autoscaler.json \
-    --save=~/Projects/autoscaled-masterkube-aws/config/aws-ca-k8s/autoscaler-state.json \
+./out/linux/$GOARCH/aws-autoscaler \
+    --config=$HOME/Projects/autoscaled-masterkube-aws/config/aws-ca-k8s/config/kubernetes-aws-autoscaler.json \
+    --save=$HOME/Projects/autoscaled-masterkube-aws/config/aws-ca-k8s/config/autoscaler-state.json \
     --log-level=info
